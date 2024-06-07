b3562924b92812d39e314906f6e37e1a6037d151
sbx_user1051 <sbx_user1051@169.254.86.53>
2024-06-07 21:34:28 +0000

    Fri Jun 07 21:34:28 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -36,23 +36,24 @@ public class CheckoutSolution {
        {
            total=total+45*(parb/2);
        }
-       if(para%3!=0 && parb%2!=0 )
-       {
-           total=total+para*50 + parb+30 + parc*20 + pard*15;
-       }
         if(para%3==0 && parb%2!=0 )
         {
             total=total+ parb+30 + parc*20 + pard*15;
         }
-        if(para%3!=0 && parb%2==0 )
+        else if(para%3!=0 && parb%2==0 )
         {
             total=total+para*50 + parc*20 + pard*15;
+        }
+        else
+        {
+            total=total+para*50 + parb+30 + parc*20 + pard*15;
         }
         //System.out.print('\n');
         return total;
 
     }
 }
+
 
 
 
