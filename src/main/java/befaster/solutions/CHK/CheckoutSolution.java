50b409b036f9a17ea2ee739757b7df7f6d886fd6
sbx_user1051 <sbx_user1051@169.254.86.53>
2024-06-07 20:48:42 +0000

    Fri Jun 07 20:48:42 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,45 +3,15 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        int parA=0;
-        int parb=0;
-        int parc=0;
-        int pard=0;
-        int total;
-        for(int i=0; i<skus.length(); i++ )
-        {
-            if(skus[i]=="A")
-            {
-                parA++;
-            }
-            if(skus[i]=="B")
-            {
-                parb++;
-            }
-            if(skus[i]=="C")
-            {
-                parc++;
-            }
-            if(skus[i]=="D")
-            {
-                pard++;
-            }
-        }
-        if(parA==3)
-        {
-            total=total+130;
-            total=total+parb*30+parc*20+pard*15;
-        }
-        else if(parb==2)
-        {
-            total=total+45;
-            total=total+parA*50+parc*20+pard*15;
-        }
-        else {
-            total=parA*50+parb*30+parc*20+pard*15;
-        }
+    public Integer checkout(String skus)
+    {
+        int total=0;
+       for(int i=0;i<skus.length();i++)
+       {
+           System.out.println(skus.charAt(i));
+       }
         return total;
     }
 }
+
 
