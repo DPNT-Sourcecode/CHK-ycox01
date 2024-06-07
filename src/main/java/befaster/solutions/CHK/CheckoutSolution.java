8635def28abb0e425a438e34092077b4a9866001
sbx_user1051 <sbx_user1051@169.254.86.53>
2024-06-07 20:46:03 +0000

    Fri Jun 07 20:46:03 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,6 +4,44 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        
+        int parA=0;
+        int parb=0;
+        int parc=0;
+        int pard=0;
+        int total;
+        for(int i=0; i<skus.length(); i++ )
+        {
+            if(skus[i]=="A")
+            {
+                parA++;
+            }
+            if(skus[i]=="B")
+            {
+                parb++;
+            }
+            if(skus[i]=="C")
+            {
+                parc++;
+            }
+            if(skus[i]=="D")
+            {
+                pard++;
+            }
+        }
+        if(parA==3)
+        {
+            total=total+130;
+            total=total+parb*30+parc*20+pard*15;
+        }
+        else if(parb==2)
+        {
+            total=total+45;
+            total=total+parA*50+parc*20+pard*15;
+        }
+        else {
+            total=parA*50+parb*30+parc*20+pard*15;
+        }
+        return total;
     }
 }
+
