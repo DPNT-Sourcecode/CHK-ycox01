c4cbe21e72cb4eb9e670e15bc4d7ab713fb6c5f6
sbx_user1051 <sbx_user1051@169.254.86.53>
2024-06-07 20:18:15 +0000

    Fri Jun 07 20:18:15 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,10 +8,23 @@ public class CheckoutSolution {
         for(int i=0;i<skus.length();i++) {
            if(skus[i]=='A')
            {
-               total=total+
+               total=total+50;
+           }
+           else if(skus[i]=="B")
+           {
+               total=total+30;
+           }
+           else if(skus[i]=="C")
+           {
+               total=total+20;
+           }
+           else
+           {
+               total=total+10;
            }
 
         }
         return total;
     }
 }
+
